baad2e49e24f94c09a2f73a38bfbdf15014c53e7
Edan Weis <edanweis@gmail.com>
2016-04-06 16:30:52 +1000

    added flask_restfuj

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Hello(Resource):
+	def get(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('location', type=str, required=False)
+		args = parser.parse_args()
+		# auth_payload = {"uid": args['location']}
+		token = args['token']
+		headers = {
+		    "Authorization": "Bearer %s" % token,
+		}
+		response = requests.get('https://api.lifx.com/v1/lights/all', auth=(token, ''))
+		# trigger change
+		return response.status
+		
 
-api.add_resource(HelloWorld, '/')
-
+api.add_resource(Hello, '/')
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
